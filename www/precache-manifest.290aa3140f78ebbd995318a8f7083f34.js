410d5b170040da83f62f00bc9be28803f06972fe
omer-checkmarx <omer.dayan@checkmarx.com>
2020-01-02 22:46:16 +0200

    WWW

---
+++ www/precache-manifest.290aa3140f78ebbd995318a8f7083f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4a8e347d8254880f5c934070ec4733",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01dfc4777018e6214487",
+    "url": "./static/css/main.eca9660e.chunk.css"
+  },
+  {
+    "revision": "33e44ed71c763391a7af",
+    "url": "./static/js/2.fc973af0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.fc973af0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01dfc4777018e6214487",
+    "url": "./static/js/main.d7479380.chunk.js"
+  },
+  {
+    "revision": "28b00f9b54058a166d18",
+    "url": "./static/js/runtime-main.585ec4dd.js"
+  },
+  {
+    "revision": "fe8f85f044125bd003762647dc63f3be",
+    "url": "./static/media/logo.fe8f85f0.png"
+  }
+]);
